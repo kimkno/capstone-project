f5ff0da288ab07bf801837244c6758aa8d34cfe0
kimkno <rlarbsh3@gmail.com>
2024-11-16 00:04:06 +0900

    reset config for jdk17

--- client/lib/actions/actions.dart
+++ client/lib/actions/actions.dart
@@ -1,8 +1,3 @@
-import '/backend/api_requests/api_manager.dart';
-import '/backend/backend.dart';
-import '/backend/schema/structs/index.dart';
-import '/page_ui/page_ui_theme.dart';
-import '/page_ui/page_ui_util.dart';
 import 'package:flutter/material.dart';
 
 Future requestAPI(BuildContext context) async {}
